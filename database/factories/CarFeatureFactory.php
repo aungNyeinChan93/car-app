0abba46e61bd5587a294db4869b2fb6bb17a0f28
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-02-15 09:17:02 +0630

    carseeder fix

--- database/factories/CarFeatureFactory.php
+++ database/factories/CarFeatureFactory.php
@@ -17,7 +17,18 @@ class CarFeatureFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'Air_Conditioning' => fake()->randomElement(['on', null]),
+            'Power_Windows' => fake()->randomElement(['on', null]),
+            'Power_DoorLocks' => fake()->randomElement(['on', null]),
+            'ABS' => fake()->randomElement(['on', null]),
+            'Cruise_Control' => fake()->randomElement(['on', null]),
+            'Bluetooth_Connectivity' => fake()->randomElement(['on', null]),
+            'Remote_Start' => fake()->randomElement(['on', null]),
+            'GPS' => fake()->randomElement(['on', null]),
+            'Heated_Seats' => fake()->randomElement(['on', null]),
+            'Rear_ParkingSensors' => fake()->randomElement(['on', null]),
+            'Leather_Seats' => fake()->randomElement(['on', null]),
+            'Climate_Control' => fake()->randomElement(['on', null]),
         ];
     }
 }
--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Car;
+use App\Models\CarFeature;
+use App\Models\User;
+use App\Models\CarImage;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -13,6 +16,12 @@ class CarSeeder extends Seeder
      */
     public function run(): void
     {
-        Car::factory(5)->create();
+        // Car::factory(5)->create();
+
+        Car::factory(5)
+            ->has(CarImage::factory()->count(3), 'images')
+            ->has(User::factory()->count(5), 'favouriteUsers')
+            ->has(CarFeature::factory(), 'carFeature')
+            ->create();
     }
 }
